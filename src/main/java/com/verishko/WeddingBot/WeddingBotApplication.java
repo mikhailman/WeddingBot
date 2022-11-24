b16a387f25acb215867672f04d3f03ac4c0d17e0
Michael <mishanya.v91@mail.ru>
2022-11-24 17:31:55 +0300

    initial commit first working version telegram bot with menu

---
+++ src/main/java/com/verishko/WeddingBot/WeddingBotApplication.java
@@ -0,0 +1,13 @@
+package com.verishko.WeddingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verishko/WeddingBot/WeddingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verishko.WeddingBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
